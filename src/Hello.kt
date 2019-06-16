274546d31668c5f434ddb045ec2f336e11a2871e
wanfadger <Galiwango.fahad@gmail.com>
2019-06-16 08:00:46 -0700

    classes are created using the class keyword and properties accessed using a dot

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,4 +1,12 @@
 
 fun main(args: Array<String>){
     println("Hello world , am a kotlin developer")
+
+    //we dont use the new keyword to create an instance of a class
+    //class properties are accessed using a dot
+
+    var alien = Alien()
+    alien.name = "Galiwango Fahad"
+
+    println(message = "my name is ${alien.name}");
 }
